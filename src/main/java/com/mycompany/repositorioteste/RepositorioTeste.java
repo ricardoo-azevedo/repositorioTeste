51613ed24e52391502902d9ab5114b8483e5586d
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-04-18 15:51:21 -0300

    Primeiro alteração

--- src/main/java/com/mycompany/repositorioteste/RepositorioTeste.java
+++ src/main/java/com/mycompany/repositorioteste/RepositorioTeste.java
@@ -12,5 +12,6 @@ public class RepositorioTeste {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Primeira alteração");
     }
 }
